6903ab69d2d5785ae75796712104bffa3f3d972b
flavea <flavia_pela@yahoo.com>
2021-04-17 17:50:04 +0300

    cleaned code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,13 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        File root = new File("C:/Users/flavi/IdeaProjects/firstmaven");
+        File root = new File("C:/Users/flavi/IdeaProjects/firstmaven"); //TODO take dynamically
 
         if(root.listFiles() != null) {
-            for(File f: root.listFiles()) {
-                System.out.println(f.getName());
+            for(File f: Objects.requireNonNull(root.listFiles())) {
+                System.out.println(f.getName()); //TODO replace with log
             }
         }
     }
