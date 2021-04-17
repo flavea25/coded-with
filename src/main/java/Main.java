c864591835db7c60ecd744b8675d064026d7f554
flavea <flavia_pela@yahoo.com>
2021-04-17 17:49:19 +0300

    see files from folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        File root = new File("C:/Users/flavi/IdeaProject/firstmaven");
-        System.out.println(root.getName());
+        File root = new File("C:/Users/flavi/IdeaProjects/firstmaven");
+
         if(root.listFiles() != null) {
             for(File f: root.listFiles()) {
                 System.out.println(f.getName());
