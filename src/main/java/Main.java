04ff797a573e1ea441cce247124940e26f1e8c0f
flavea <flavia_pela@yahoo.com>
2021-04-17 17:38:48 +0300

    first try for files in folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        File root = new File("C:/Users/flavi/IdeaProject/firstmaven");
+        System.out.println(root.getName());
+        if(root.listFiles() != null) {
+            for(File f: root.listFiles()) {
+                System.out.println(f.getName());
+            }
+        }
     }
 }
