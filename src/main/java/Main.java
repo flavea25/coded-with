73b5027844538afcccb59513f059126a46fe2144
flavea <flavia_pela@yahoo.com>
2021-04-18 14:50:31 +0300

    printed all file names from a folder recursively

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
-import java.io.File;
-import java.util.Objects;
+import services.FileService;
 
 public class Main {
-    public static void main(String[] args) {
-        File root = new File("C:/Users/flavi/IdeaProjects/firstmaven"); //TODO take dynamically
 
-        if(root.listFiles() != null) {
-            for(File f: Objects.requireNonNull(root.listFiles())) {
-                System.out.println(f.getName()); //TODO replace with log
-            }
-        }
+    public static void main(String[] args) {
+        FileService fileService = new FileService();
+
+//        fileService.printFileLines("C:/Users/flavi/IdeaProjects/firstmaven/pom.xml");
+        fileService.printAllFilesFromFolder("C:/Users/flavi/IdeaProjects/firstmaven","");
     }
 }
