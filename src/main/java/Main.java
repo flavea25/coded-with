8b8802358652d5f0ab229f305579bd5a2b959fa3
flavea <flavia_pela@yahoo.com>
2021-08-12 19:07:55 +0300

    get links/paths from args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,14 @@ public class Main {
     //ROOT: "C:/Users/flavi/IdeaProjects/firstmaven"
     //SVV: "C:/Users/flavi/git/SVV"
     public static void main(String[] args) { //TODO take links/paths as arguments
-        TechnologyService technologyService = Guice.createInjector(new MyInjector()).getInstance(TechnologyService.class);
+        if(args.length != 2) {
+            log.error("Not enough arguments!! - Please pass:\n\t 1. Path/link to analyzed project, and \n\t 2. Path to a JSON file containing the searched-for technologies!");
+        }
+        else {
+            TechnologyService technologyService = Guice.createInjector(new MyInjector()).getInstance(TechnologyService.class);
 
-        technologyService.getUsedTechnologiesByCategory("https://github.com/flavea25/SVV", technologyService.getAllTechnologiesFromFile("C:/Users/flavi/IdeaProjects/firstmaven/src/main/resources/technologies.json"))
-                .forEach((category, technologies) -> log.info(category.name() + ": " + technologyService.getTechnologiesNames(technologies)));
+            technologyService.getUsedTechnologiesByCategory(args[0], technologyService.getAllTechnologiesFromFile(args[1]))
+                    .forEach((category, technologies) -> log.info(category.name() + ": " + technologyService.getTechnologiesNames(technologies)));
+        }
     }
 }
